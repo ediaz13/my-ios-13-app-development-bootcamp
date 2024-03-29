722c888a09f211c705cf46e6f86de53cfdd4702e
ediaz13 <bbadmin@MBP-de-BBADmin.fibertel.com.ar>
2024-03-29 20:07:55 -0300

    Im rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by ediaz13 on 29/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
