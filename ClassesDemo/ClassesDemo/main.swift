b6164483c0b58a362bff1ebc6cba56aa67621a88
ediaz13 <bbadmin@MBP-de-BBADmin.fibertel.com.ar>
2024-04-15 22:55:54 -0300

    Classes vs Structs

--- ClassesDemo/ClassesDemo/main.swift
+++ ClassesDemo/ClassesDemo/main.swift
@@ -5,10 +5,17 @@
 //  Created by ediaz13 on 12/04/2024.
 //
 
-let skeleton = Enemy()
+let skeleton = Enemy(health: 100, attackStrength: 10)
+//Structs create new objets
+//Structs are passed by Values
+//Classes are passed by Reference
+//Create Structs by Default!
+//Structs are inmutable
+
 print(skeleton.health)
 skeleton.move()
 skeleton.attack()
+skeleton.takeDanage(amount: 10)
 
 let dragon = Dragon()
 dragon.attack()
