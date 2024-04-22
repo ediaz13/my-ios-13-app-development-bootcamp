7abf51fb0f9d0189ab7c928906a9e5e89cc23f17
ediaz13 <bbadmin@MBP-de-BBADmin.fibertel.com.ar>
2024-04-22 20:44:36 -0300

    Optional Binding

---
+++ OptionalsDemo/OptionalsDemo/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  OptionalsDemo
+//
+//  Created by ediaz13 on 22/04/2024.
+//
+
+let myOptional: String?
+
+myOptional = nil
+
+let text: String = myOptional ?? "I am the default value, Mogul of course"
+
+
